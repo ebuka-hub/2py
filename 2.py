9dd3ea7fd55e15cb62c2681f12aa3cab9554436f
ebuka-hub <albertebuka69@gmail.com>
2025-03-28 08:58:48 -0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.Screen().bgcolour("orange")
+board = turtle.Turtle()
+
+#first triangle for star
+board.forward(180)#draw base
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+borad.forward(100)
+
+board.penup
